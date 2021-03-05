3c15cac6f211b0ededa7a63d948b6952a78ef9f3
chethanbr86 <chethanbr86@gmail.com>
2021-03-05 12:04:23 +0530

    Update min_max_sum.py

--- min_max_sum.py
+++ min_max_sum.py
@@ -1,3 +1,5 @@
+#Unsolved
+
 arr = [1,3,5,7,9]
 
 def miniMaxSum(arr):
@@ -6,7 +8,7 @@ def miniMaxSum(arr):
     for i in arr:
         if i < a:
             a = i
-            total = sum(arr.remove(a))
+        total = sum(arr.pop(a))
     return total
 
 print(miniMaxSum(arr))
