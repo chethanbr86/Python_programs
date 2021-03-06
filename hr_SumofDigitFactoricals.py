7c3f435f095e53a3ee60e77767b19ee6cdb55d91
chethanbr86 <59503862+chethanbr86@users.noreply.github.com>
2021-03-06 14:59:56 +0530

    Update hr_SumofDigitFactoricals.py

--- hr_SumofDigitFactoricals.py
+++ hr_SumofDigitFactoricals.py
@@ -4,11 +4,11 @@ from functools import reduce
 
 # num = 351
 
-#myway
+#1st way
 # num_list = list(map(int, str(num)))
 # print(num_list)
 
-#otherway
+#2nd way
 # def split_num(n):
 #     s = list(str(n))
 #     return [int(i) for i in s]
@@ -19,13 +19,13 @@ def split_num(n):
 
 print(split_num(342))
 
-#myway
+#1st way
 # fact1 = reduce(lambda x, y: x*y, range(1, num_list[0]+1))
 # fact2 = reduce(lambda x, y: x*y, range(1, num_list[1]+1)) 
 # total = fact1+fact2
 # print(total)
 
-#otherway
+#2nd way
 # def fact(n):
 #     if n > 1:
 #         return n*fact(n-1)    
@@ -39,16 +39,16 @@ def fact(n):
     else:
         return 1
 
-#myway
+#1st way
 # sum = 0
 # num_list2 = list(map(int, str(total)))
 # for i in num_list2:
 #     sum += i
 # print(sum)
 
-#otherway
+#2nd way
 def f(n):
-    return sum([fact(k) for k in split_num(n)])    #return sum([fact(k) for k in num_list]) #if myway is used at start
+    return sum([fact(k) for k in split_num(n)])    #return sum([fact(k) for k in num_list]) #if 1st way is used at start
 
 print(f(342))
 
