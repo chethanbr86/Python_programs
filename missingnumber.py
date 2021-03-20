5922b2c21a345d6f49341196b35c3ebf8654402a
chethanbr86 <chethanbr86@gmail.com>
2021-03-20 16:30:57 +0530

    Create missingnumber.py

---
+++ missingnumber.py
@@ -0,0 +1,23 @@
+def missingNumber(nums):
+    nums.sort()
+    #print(nums[0])
+    
+    # for i in range(len(nums)):            
+    #     if ((i+1) - i) != 1:            
+    #         break
+    # return nums[i]-1
+    
+    if nums[0] != 0:
+        return 0
+    
+    if nums[-1] == len(nums):
+        for i in range(len(nums)):            
+            if ((i+1) - i) != 1 or (i - (i-1)) != 1:            
+                break
+        return nums[i]-1        
+    else:
+        return nums[-1]+1
+           
+print(missingNumber([0,3,2]))
+
+#Yet to solve
