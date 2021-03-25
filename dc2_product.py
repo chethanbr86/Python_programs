8b5f2ab5dab4f01600dad74c85f715942e702959
chethanbr86 <chethanbr86@gmail.com>
2021-03-25 20:52:57 +0530

    Update dc2_product.py

--- dc2_product.py
+++ dc2_product.py
@@ -1,8 +1,20 @@
 my_list = [1,2,3,4,5]
+#my_list2 = my_list
 
-for i in range(len(my_list)):
-    for j in range(len(my_list)):
-        print(i[-1]*j)
+# for i in range(len(my_list)):    
+#         print(my_list[i]*my_list[i])
+
+# sum1 = []
+# for i in my_list:
+#     sum2 = []
+#     for j in my_list:
+#         sum1.append(i*j)
+#         sum2.append(i*j)
+#     print(sum2)
+# print(sum1)
+
+
+
 
 # not solved
         
