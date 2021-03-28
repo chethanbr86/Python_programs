95c60f4f6c81ce47c6ea7d4fa99c52b3c88ce7af
chethanbr86 <chethanbr86@gmail.com>
2021-03-28 17:01:05 +0530

    Create above_Average.py

---
+++ above_Average.py
@@ -0,0 +1,18 @@
+my_list = [ [2,4,6,8,10,12], 6.5]
+m = my_list[0]
+n = my_list[1]
+
+def sumoflist(m):
+    total = 0
+    for i in m:
+        total = total + i
+    return total
+
+# avg = sum(m)/len(m)
+    
+avg = sumoflist(m)/len(m)
+
+if n > avg:
+    print(True)
+else:
+    print(False)
