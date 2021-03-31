0544fe165bc9bc30af327756a24ece940a969625
chethanbr86 <chethanbr86@gmail.com>
2021-03-31 21:18:27 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,17 @@
 word = input()
 word_lower = word.lower()
 
-palin = word[::-1]
-if palin == word_lower:
+# palin = word[::-1]
+# if palin == word_lower:
+#     print('Palindrome')
+# else:
+#     print('Not a Palindrome')
+
+palin = ''
+for i in word_lower:
+    palin = i + palin
+
+if palin.lower() == word_lower:
     print('Palindrome')
 else:
     print('Not a Palindrome')
