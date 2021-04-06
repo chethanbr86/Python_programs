b10c0e965ec61426863124c20e8557ebd47213cd
chethanbr86 <chethanbr86@gmail.com>
2021-04-06 21:26:18 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -8,7 +8,7 @@ def anagram(s1,s2):
     if len(s1) != len(s2):
         return False
     for i in range(len(s1)):
-        if s1.count(s1[i]) != s2.count(s1[i]):
+        if s1.count(s1[i]) != s2.count(s1[i]): #did not understand funda correctly
             return False
         print(s1[i],s2[i])
         print(s1.count(s1[i]),s2.count(s1[i]))
