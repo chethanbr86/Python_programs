5d298c8dea975f90b8bccc1981256c7771029cad
chethanbr86 <chethanbr86@gmail.com>
2021-04-06 21:25:45 +0530

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,17 @@
+s1 = 'thing'
+s2 = 'night'
+
+# l1 = len(s1)
+# l2 = len(s2)
+
+def anagram(s1,s2):
+    if len(s1) != len(s2):
+        return False
+    for i in range(len(s1)):
+        if s1.count(s1[i]) != s2.count(s1[i]):
+            return False
+        print(s1[i],s2[i])
+        print(s1.count(s1[i]),s2.count(s1[i]))
+    return True
+
+print(anagram(s1,s2))
