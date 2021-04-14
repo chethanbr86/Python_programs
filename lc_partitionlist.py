1dcded6b65ca46e35d7479f830976bc0462995f0
chethanbr86 <chethanbr86@gmail.com>
2021-04-14 15:17:38 +0530

    Update lc_partitionlist.py

--- lc_partitionlist.py
+++ lc_partitionlist.py
@@ -1,6 +1,6 @@
 #Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
 #You should preserve the original relative order of the nodes in each of the two partitions.
-head = [1,4,3,2,5,2]
+head = [1,4,3,2,6,5,2,6]
 x = 3
 
 #output = [1,2,2,4,3,5]
@@ -8,16 +8,16 @@ x = 3
 def partition(head, x):
     output = []
     for i in head:
-        if i < x:
-            head.pop(i)
+        if i < x:            
             output.append(i)
+            head.remove(i)
         else:
             continue
-    #print(head, output)
+    output = output + head
     return output
 
 print(partition(head,x))
 
-#unfinished
+
                 
 
