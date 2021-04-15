4d88e108f35cc81b29b35b0d0da94b4507c0517f
chethanbr86 <chethanbr86@gmail.com>
2021-04-15 18:09:00 +0530

    Update lc_sumoftwonumbers.py

--- lc_sumoftwonumbers.py
+++ lc_sumoftwonumbers.py
@@ -1,11 +1,22 @@
+# def twoSum(nums, target):
+#     new_list = []
+#     for i in range(len(nums)):
+#         for j in range(len(nums)):
+#             if nums[i] + nums[j] == target:
+#                 new_list.append(i)
+#                 new_list.append(j)
+#                 return new_list
+
+# print(twoSum([3,2,4], 6))
+
 def twoSum(nums, target):
     new_list = []
     for i in range(len(nums)):
-        for j in range(len(nums)):
+        for j in range(i+1,len(nums)):
             if nums[i] + nums[j] == target:
                 new_list.append(i)
                 new_list.append(j)
                 return new_list
 
-print(twoSum([2,7,4,5], 6))
+print(twoSum([3,2,4], 6))
 
