2a189f551bb0cfe94ad590b17c8abe445527c39e
chethanbr86 <chethanbr86@gmail.com>
2021-04-29 19:44:25 +0530

    Create he_listrotation.py

---
+++ hackerearth/he_listrotation.py
@@ -0,0 +1,12 @@
+k = 2
+l = [1, 2, 3, 4, 5]
+
+# a = l[0]
+# while k:
+#     for i in range(len(l)):
+#         if l[i] > a:
+#             a = i 
+#         else:
+#             continue
+
+print('jeldfs')
