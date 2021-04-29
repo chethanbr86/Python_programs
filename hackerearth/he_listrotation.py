f6ad4eb2ef4bd773d8b6d543806e59ee180f3363
chethanbr86 <chethanbr86@gmail.com>
2021-04-29 20:14:02 +0530

    Update he_listrotation.py

--- hackerearth/he_listrotation.py
+++ hackerearth/he_listrotation.py
@@ -1,12 +1,16 @@
 k = 2
 l = [1, 2, 3, 4, 5]
 
-# a = l[0]
-# while k:
-#     for i in range(len(l)):
-#         if l[i] > a:
-#             a = i 
-#         else:
-#             continue
+a = l[0]
+new = 0
 
-print('jeldfs')
+for i in range(len(l)):
+    if l[i] > a:
+        temp = l[i]
+        l[i] = l[new]
+        l[new] = temp
+        new += 1
+
+print(l)
+#not completed
+# https://www.geeksforgeeks.org/move-negative-numbers-beginning-positive-end-constant-extra-space/
