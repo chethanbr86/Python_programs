9f513ddecd20fec2c4e9ce899a2a0e50ae2ae0b4
chethanbr86 <chethanbr86@gmail.com>
2021-05-06 15:44:38 +0530

    Update he_vowel.py

--- hackerearth/he_vowel.py
+++ hackerearth/he_vowel.py
@@ -1,26 +1,20 @@
-name2 = 'omahgoTuRuLob'
-name3 = 'OmAhgotUrulobEI'
-name4 = 'aeKORONAoiBATCHu'
+# hackerearth solution
 
-
-name = [name2,name3,name4]
-vow1 = ['A','E','I','O','U']
-vow2 = ['a','e','i','o','u']
-
-def vowel(name, vow1, vow2):
-    for i in range(len(name)):
-        # for j in vow:
-        if name[i] in vow1 or name[i] in vow2:  
-            return 1
-        else:
-            return 0
-
-
-m = vowel(name, vow1, vow2)
-for i in name:
-    if m == 1:
-        print('lovely string')
+def solve(s):
+    vowels = set(['a', 'e', 'i', 'o', 'u'])
+    flag1 = True
+    flag2 = True
+    for i in vowels:
+        if i not in s:
+            flag1 = False
+        if i.upper() not in s:
+            flag2 = False
+    return flag1 or flag2
+ 
+n = int(input())
+for i in range(n):
+    s = input()
+    if solve(s):
+        print("lovely string")
     else:
-        print('ugly string')
-
-#unsolved
+        print("ugly string")
