c884d443cf8e21bf206acdc933bd49ea65b6c1fb
chethanbr86 <chethanbr86@gmail.com>
2021-05-07 13:34:03 +0530

    Update he_listrotation.py

--- hackerearth/he_listrotation.py
+++ hackerearth/he_listrotation.py
@@ -19,18 +19,18 @@
 
 # This is the answer to the above question
 n = int(input())
-for i in range(n):
-    m = list(map(int,input().split()))
-    my_list = list(map(int,input().split()))
+# for i in range(n):
+m = list(map(int,input().split()))
+my_list = list(map(int,input().split()))
 
 len_list = m[0]
 k = m[1]
 
 new_list = []
-# for j in range(n):
-for i,x in enumerate(my_list):
-    new_list.append(my_list[(i+(k+1))*n%len_list])
-
+for j in range(n):
+    for i,x in enumerate(my_list):
+        new_list.append(my_list[(i+(k+1))%len_list])
+print(*new_list, sep = " ")
 # for j in range(n+1):
 #     for i,x in enumerate(my_list):
 #         new_list.append(my_list[(i+(k+1))*j%len_list])
@@ -38,7 +38,7 @@ for i,x in enumerate(my_list):
 # print(new_list)
 # This is how we provide required solution without list
 # for i in range(n):
-print(*new_list, sep = " ")
+#     print(*new_list, sep = " ")
 
 #Solution not according to question page
 # https://www.hackerearth.com/practice/codemonk/
