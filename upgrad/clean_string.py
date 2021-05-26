75327da19fb59a2b602fd5425716f12abe5d4d57
chethanbr86 <chethanbr86@gmail.com>
2021-05-26 21:06:21 +0530

    Update clean_string.py

--- upgrad/clean_string.py
+++ upgrad/clean_string.py
@@ -1,13 +1,27 @@
 # Remove numbers and special characters and reverse upper and lower strings
 
-import re
-inp = 'cHETU123'
+#import re
+inp = 'cHETU123!'
+inp = list(inp)
 
-pattern = r'[0-9]'
+#pattern = r'[0-9]'
 
 # Match all digits in the string and replace them with an empty string
-out = re.sub(pattern, '', inp)
+#out = re.sub(pattern, '', inp)
 
-print(out)
+#print(out)
+
+
+
+# Another way without using regex
+
+# Learnings
+# This is how you check every element data type in list
+#ls = [type(item) for item in inp]
+#print(ls)
+#EndLearnings
+
+new_string = ''.join(filter(lambda item: item.isalpha(), inp))
+print(new_string)
 
 #complete it with replacing lower with upper and upper with lower
