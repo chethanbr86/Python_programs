f795750d10fe4141249e53ed41f47cee4469a193
chethanbr86 <59503862+chethanbr86@users.noreply.github.com>
2021-11-22 15:31:09 +0530

    Update forloop.py

--- datastructure/forloop.py
+++ datastructure/forloop.py
@@ -1,5 +1,4 @@
 li = [5,3,8,2,4,6,10]
-ran = [4,9]
 
 for i in li:
     print(i)
@@ -7,3 +6,7 @@ for i in li:
 for j in range(len(li)):
     print('range is ', j)
     print('number is', li[j])
+    
+for i in range(len(li)):
+    for j in range(len(li)):
+        print(i, li[i], j, li[j])
