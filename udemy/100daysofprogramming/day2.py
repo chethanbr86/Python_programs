59013237696857b742ba4f2a81ca00190cb3c708
chethanbr86 <chethanbr86@gmail.com>
2021-12-23 09:59:03 +0530

    Create day2.py

---
+++ udemy/100daysofprogramming/day2.py
@@ -0,0 +1,25 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+#HINT 1: https://www.google.com/search?q=how+to+round+number+to+2+decimal+places+python&oq=how+to+round+number+to+2+decimal
+#HINT 2: https://www.kite.com/python/answers/how-to-limit-a-float-to-two-decimal-places-in-python
+print('Welcome to tip calculator!')
+bill = float(input('What was the total bill? $'))
+ppl = int(input('How many people are sharing the bill? '))
+tip = int(input("What's the percentage of tip? "))
+bill_per_person = 0
+bill_per_person = round((((bill*tip/100)+bill)/ppl),2)
+print(f'Bill payable per person is: ${bill_per_person:.2f}')
+
+#udemy way
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill?"))
+
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
