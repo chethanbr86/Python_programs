d1d437bac25261e81c4ff1aec81ae131c2408912
chethanbr86 <chethanbr86@gmail.com>
2021-12-23 09:59:20 +0530

    Create day_3_3.py

---
+++ udemy/100daysofprogramming/day_3_3.py
@@ -0,0 +1,31 @@
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if year%4 == 0 :	
+	if year%400 == 0 and year%100 == 0:
+		print('Leap')
+	elif year%400 == 0:
+		print('Leap')
+	elif year%100 == 0:	
+		print('Not Leap')
+	else:
+		print('Leap')
+else:
+	print('Not Leap')
+
+'''
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+'''
+
+#tips and tricks: instead of repeating, go for multiple nested
