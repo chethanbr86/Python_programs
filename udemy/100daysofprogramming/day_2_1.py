b504e551b0ac9cb3197b1b74093001028e449c11
chethanbr86 <chethanbr86@gmail.com>
2021-12-23 09:40:21 +0530

    Update day_2_1.py

--- udemy/100daysofprogramming/day_2_1.py
+++ udemy/100daysofprogramming/day_2_1.py
@@ -5,18 +5,26 @@ two_digit_number = input("Type a two digit number: ")
 
 ####################################
 #Write your code below this line 👇
-print(type(two_digit_number))
-print(len(two_digit_number))
+#myway
+# print(type(two_digit_number))
+# print(len(two_digit_number))
 
-def dig_split(n):
-    if len(n) != 2:
-        return 0
-    return list(map(int,str(n)))
+# def dig_split(n):
+#     if len(n) != 2:
+#         return 0
+#     return list(map(int,str(n)))
 
-if 0:
-    print('Please print 2 digit number')
-else:
-    print(dig_split(two_digit_number))
-    print(sum(dig_split(two_digit_number)))
+# if 0:
+#     print('Please print 2 digit number')
+# else:
+#     print(dig_split(two_digit_number))
+#     print(sum(dig_split(two_digit_number)))
 
     #fix this
+
+#udemy way
+
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+
+print(int(first_digit)+int(second_digit))
