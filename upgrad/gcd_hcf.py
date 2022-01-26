2f0f0b776ec72251d20a7b089f98b684493b0e1f
chethanbr86 <chethanbr86@gmail.com>
2022-01-26 21:23:01 +0530

    Update gcd_hcf.py

--- upgrad/gcd_hcf.py
+++ upgrad/gcd_hcf.py
@@ -10,7 +10,7 @@ def gcd(a,b):
     return gcd(a, b%a)
 
 inp = input()
-a,b = map(int,inp.split(',')) #imp
+a,b = map(int,inp.split(',')) #imp #myway
 print(a,b,type(a),type(b))
 if gcd(a,b):
     print('GCD of ', a, 'and', b, 'is', gcd(a,b))
