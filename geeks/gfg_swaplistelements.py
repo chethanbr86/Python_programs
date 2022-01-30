d26b201b18725b36d132738f86e6f4743c7e1c4f
chethanbr86 <chethanbr86@gmail.com>
2022-01-30 11:34:31 +0530

    Update gfg_swaplistelements.py

--- geeks/gfg_swaplistelements.py
+++ geeks/gfg_swaplistelements.py
@@ -1,16 +1,42 @@
 # Swap first and last element of list
 from re import X
 
+#Method 1: 
+# def swap(new_list):
+#     x = new_list[0]
+#     new_list[0] = new_list[-1]
+#     new_list[-1] = x
+#     return new_list
 
-inp = [12, 35, 9, 56, 24]
-
-#Method1: swap variables
-x = inp[0]
-inp[0] = inp[-1]
-inp[-1] = x
-
-print(inp)
+# inp = [12, 35, 9, 56, 24]
+# print(swap(inp))
 
 #Method 2:
+# def swap(new_list):
+#     new_list[0], new_list[-1] = new_list[-1], new_list[0]
+#     return new_list
 
+# inp = [12, 35, 9, 56, 24]
+# print(swap(inp))
 
+#Method 3:
+# def swap(new_list):
+#     start,*middle,end = new_list
+#     new_list = [end, *middle, start]
+#     return new_list
+
+# inp = [12, 35, 9, 56, 24]
+# print(swap(inp))
+
+#Method 4:
+def swap(new_list):
+    first = new_list.pop(-1) #0
+    last = new_list.pop(0) #-1
+
+    new_list.insert(0, first) #last
+    new_list.append(last) #first
+
+    return new_list
+
+inp = [12, 35, 9, 56, 24]
+print(swap(inp))
