602cd24ec6de4fee31e0e9fbb6d9c70026883960
chethanbr86 <chethanbr86@gmail.com>
2022-01-30 11:10:02 +0530

    Update gfg_swaplistelements.py

---
+++ geeks/gfg_swaplistelements.py
@@ -0,0 +1,16 @@
+# Swap first and last element of list
+from re import X
+
+
+inp = [12, 35, 9, 56, 24]
+
+#Method1: swap variables
+x = inp[0]
+inp[0] = inp[-1]
+inp[-1] = x
+
+print(inp)
+
+#Method 2:
+
+
