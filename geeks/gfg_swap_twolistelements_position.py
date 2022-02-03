3449676fd6fc2bf0b5d53cb1b52605da1c7e570f
chethanbr86 <chethanbr86@gmail.com>
2022-02-03 15:19:39 +0530

    Update gfg_swap_twolistelements_position.py

--- geeks/gfg_swap_twolistelements_position.py
+++ geeks/gfg_swap_twolistelements_position.py
@@ -4,14 +4,11 @@ def swap(l1,p1,p2):
 
 inp = [23, 65, 19, 90]
 pos = list(map(int,input().split(',')))
-# print(pos)
-# print(len(inp))
 p1 = pos[0]
 p2 = pos[1]
 
-for i in pos:
-    if i > len(inp) or i < 1:
-        print('Input again')
-        break
-    # else: # answer is right but loop being run twice, check and debug
+# for i in pos: #no need of for loop
+if p1 > len(inp) or p1 < 1 or p2 > len(inp) or p2 < 1: 
+    print('Input again')
+else:
     print('Solution is:', swap(inp,p1-1,p2-1))
