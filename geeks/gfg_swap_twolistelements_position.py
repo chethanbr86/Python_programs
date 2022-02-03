53900891eb3bd88f57335e184527a8b322b616d5
chethanbr86 <chethanbr86@gmail.com>
2022-02-03 15:09:07 +0530

    Update gfg_swap_twolistelements_position.py

--- geeks/gfg_swap_twolistelements_position.py
+++ geeks/gfg_swap_twolistelements_position.py
@@ -12,5 +12,6 @@ p2 = pos[1]
 for i in pos:
     if i > len(inp) or i < 1:
         print('Input again')
-    else: # answer is right but loop being run twice, check and debug
-        print('Solution is:', swap(inp,p1-1,p2-1))
+        break
+    # else: # answer is right but loop being run twice, check and debug
+    print('Solution is:', swap(inp,p1-1,p2-1))
