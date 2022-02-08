f1ce6390395e0db06b84c1a1268fd0bfead46d60
chethanbr86 <chethanbr86@gmail.com>
2022-02-08 21:12:05 +0530

    Update day4_3.py

--- udemy/100daysofprogramming/day4_3.py
+++ udemy/100daysofprogramming/day4_3.py
@@ -1,16 +1,17 @@
 row1 = ["⬜️","⬜️","⬜️"]
 row2 = ["⬜️","⬜️","⬜️"]
 row3 = ["⬜️","⬜️","⬜️"]
-map = [row1, row2, row3]
+cap = [row1, row2, row3]
+print(cap)
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
-horizontal = int(position[0])
+horizontal = int(position[0]) #this is an example of taking inputs without split or giving inputs without comma
 vertical = int(position[1])
-print(map[horizontal-1])
-print(map[vertical-1])
+selected_row = cap[vertical-1]
+selected_row[horizontal-1] = "X"
 
 
 #Write your code above this row 👆
