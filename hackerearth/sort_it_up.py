39f1b545204c6e6bd8e6542489bbf666deaff2d5
chethanbr86 <chethanbr86@gmail.com>
2022-02-22 23:19:17 +0530

    update

--- hackerearth/sort_it_up.py
+++ hackerearth/sort_it_up.py
@@ -1,14 +1,17 @@
 #Asked by microsoft and facebook 
 #sove by time complexity 0(n)
 
+# https://www.hackerearth.com/practice/interviews/
+
 arr = [2,1,0,2,0] #list(map(int,input().split(',')))
 # print(sorted(arr))
 arr_new = []
 for i in range(len(arr)):
-    if arr[-1] < arr[0]:
-        arr_new.append(arr[-1])
-        arr.pop(-1)
-    else:
+    if arr[0] >= arr[-1]:
         arr_new.append(arr[0])
         arr.pop(0)
+    else:
+        arr_new.append(arr[-1])
+        arr.pop(-1)
 print(arr_new)
+#solve using for loop, while loop and use function
