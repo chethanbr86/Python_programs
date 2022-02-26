224656c6a2645b16b73916bdb3b1aa1097c99fe6
chethanbr86 <chethanbr86@gmail.com>
2022-02-26 14:44:00 +0530

    updated

--- codechef/cc_atm.py
+++ codechef/cc_atm.py
@@ -3,6 +3,9 @@
 # 3 5 3 2 1 #1st cycle accessing directly
 # 4 6 #2nd cycle same as above
 # 10 8 6 4 2nd cycle same as above
+# sol:
+# 11010
+# 0010
 
 t = int(input())
 for i in range(t):
@@ -13,6 +16,6 @@ for i in range(t):
     for j in withdraw: #this is a way of taking input but not storing in any variables but directly accessing from list
         if j <= money_units:
             money_units -= j
+            print(1, end='')
         else:
-            print('Not enough cash')
-    print('Money units left: ',money_units)
+            print(0, end='')
