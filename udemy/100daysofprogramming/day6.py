3bf206e8b33e0c4e4ac4532e507bb21eba208531
chethanbr86 <chethanbr86@gmail.com>
2022-03-24 21:31:49 +0530

    new

--- udemy/100daysofprogramming/day6.py
+++ udemy/100daysofprogramming/day6.py
@@ -1,1 +1,16 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if wall_in_front():
+        if right_is_clear():
+            turn_right()
+        else:
+            turn_left()            
+    else:
+        move()
+
+#sometimes loops
