66819b95f133f7f321fad9a2e5f0be0e4dae124b
chethanbr86 <chethanbr86@gmail.com>
2022-03-31 16:57:10 +0530

    new

--- udemy/100daysofprogramming/day6.py
+++ udemy/100daysofprogramming/day6.py
@@ -1,17 +1,37 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 #myway
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
     
-while not at_goal():
-    if wall_in_front():
-        if right_is_clear():
-            turn_right()
-        else:
-            turn_left()            
-    else:
-        move()
+# while not at_goal():
+#     if wall_in_front():
+#         if right_is_clear():
+#             turn_right()
+#         else:
+#             turn_left()            
+#     else:
+#         move()
 
 #sometimes loops
+
+#udemy
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left() 
+
+# also gives endless loops
+#As per instructor, you can download zip file and start debugging to stop endless loop (intermediate python)
+#implies both methods are correct
+
