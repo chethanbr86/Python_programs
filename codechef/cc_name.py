ffd19132f9df4e3df90423ee37ac83ec6464feea
chethanbr86 <chethanbr86@gmail.com>
2022-04-11 15:43:12 +0530

    update

--- codechef/cc_name.py
+++ codechef/cc_name.py
@@ -7,7 +7,7 @@ parents = list(map(str, input().split()))
 no_child = int(input())
 children = list(map(str, input().split()))
 
-# print(t, parents, no_child, names)
+print(t, parents, no_child, children)
 
 #myway - finding length of each list element and summing it up
 def total_len(df):
@@ -19,16 +19,21 @@ def total_len(df):
 # print(total_len(parents))
 # print(total_len(children))
 
-par_spell = ''
+par_spell = []
 for i in parents:
     par = []
     for j in i:
-        # par_spell += j
         par.append(j)
-    print(par)
+    # print(par)
+    par_spell.append(par)
+# print(par_spell)
+
+# alpha = list(chain.from_iterable(par_spell))
+#or
+alpha = [j for i in par_spell for j in i]
+# print(alpha)
 
 # if total_len(parents) <= total_len(children):
 #     print('NO')
 # else:
-
-
+    
