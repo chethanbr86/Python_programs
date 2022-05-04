61ce7232c96160094bdcfb5b7c05fa6dd8b1658f
chethanbr86 <chethanbr86@gmail.com>
2022-05-04 14:02:37 +0530

    findtarget

--- leetcode/lc_findtarget.py
+++ leetcode/lc_findtarget.py
@@ -2,11 +2,10 @@ def search(nums, target):
     for i in range(len(nums)):
         if nums[i] == target:
             return i
-        else:
-            return -1
+    return -1            
 
 nums = [-1,0,3,5,9,12]
-target = 5             
+target = 0             
 print(search(nums, target))
 
-#not solved yet
+#Different step of return - imp
