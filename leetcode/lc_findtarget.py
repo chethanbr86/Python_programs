146af2895e25ae514e488de148e6db98c92d51f9
chethanbr86 <chethanbr86@gmail.com>
2022-05-04 15:09:17 +0530

    leetcode solution

--- leetcode/lc_findtarget.py
+++ leetcode/lc_findtarget.py
@@ -1,11 +1,33 @@
-def search(nums, target):
-    for i in range(len(nums)):
-        if nums[i] == target:
-            return i
-    return -1            
+# https://leetcode.com/problems/binary-search/
 
-nums = [-1,0,3,5,9,12]
-target = 0             
-print(search(nums, target))
+# def search(nums, target):
+#     for i in range(len(nums)):
+#         if nums[i] == target:
+#             return i
+#     return -1            
+
+# nums = [-1,0,3,5,9,12]
+# target = 0             
+# print(search(nums, target))
 
 #This is not a if else but a for else problem
+
+#Solving the same by binary search
+#leetcode actual solution
+
+def search(numbs, target):
+    left = 0
+    right = len(nums)-1
+    while left <= right:
+        middle = left + (right-left)//2
+        if nums[middle] == target:
+            return middle
+        if target < nums[middle]:
+            right = middle - 1
+        else:
+            left = middle + 1
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9             
+print(search(nums, target))
