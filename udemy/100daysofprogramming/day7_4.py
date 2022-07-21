1ce261b75d357c6e4cbb3876a16fa5562e34a4dc
chethanbr86 <chethanbr86@gmail.com>
2022-07-21 12:03:34 +0530

    7_4

--- udemy/100daysofprogramming/day7_4.py
+++ udemy/100daysofprogramming/day7_4.py
@@ -74,22 +74,30 @@ for i in chosen_word:
 #Then reduce 'lives' by 1. 
 #If lives goes down to 0 then the game should stop and it should print "You lose."
 
-lives = len(chosen_word)
-print(lives)
-while '_' in display: #myway
+end_of_game = False
+# lives = len(chosen_word)  #myway won't work since there are seven stages of lives
+lives = 7
+print(f'You have {lives} lives!')
+while not end_of_game:  #'_' in display: #myway - while loop is not working, hence doing udemy way 
     guess = input().lower()
+
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
-            display[i] = chosen_word[i] #or display[i] = guess            
-    print(lives)
+            display[i] = chosen_word[i] #or display[i] = guess    
+
+    if guess not in chosen_word[i]:        
+        lives = lives - 1             
+        if lives == 0:
+            end_of_game = True
+            print('you Lost')     
+    print(lives)   
     print(display)
 
+    if '_' not in display:
+        end_of_game = True
+        print('You  Won!!!')
+        
+
 #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
-
-if '_' not in display:
-    print('You  Won!!!')
-else:
-    print('You lost!)')
-
-#not solved yet
+    print(stages[lives])
 
