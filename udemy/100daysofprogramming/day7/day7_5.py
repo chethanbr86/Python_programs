60c7fd2b11324d48a8801693632004c39deeb37f
chethanbr86 <chethanbr86@gmail.com>
2022-10-11 17:05:44 +0530

    folder restructuring

---
+++ udemy/100daysofprogramming/day7/day7_5.py
@@ -0,0 +1,54 @@
+import random
+from words import word_list
+from art import logo, stages
+
+print(logo)
+#myexplanation: There has to be 7 lives because of 7 drawings and lives not dependent on length of word
+# word_list = ["ardvark", "baboon", "camel"]
+
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
+end_of_game = False
+lives = 6
+
+chosen_word = random.choice(word_list)
+
+display = []
+for i in chosen_word:
+    display.append(i.replace(i,'_')) #myway
+
+#TODO-2: - If guess is not a letter in the chosen_word,
+#Then reduce 'lives' by 1. 
+#If lives goes down to 0 then the game should stop and it should print "You lose."
+
+# while '_' in display: #myway #myexp: if you use this, u can use break along with end_of_game else with another while statement break is not required
+while not end_of_game:
+    guess = input().lower()
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
+    
+    for i in range(len(chosen_word)):
+        if chosen_word[i] == guess:
+            display[i] = chosen_word[i] #or display[i] = guess     
+    if guess not in chosen_word:
+    # else: #else wont work here only if does
+      lives -= 1
+      print('Wrong! You have:', lives, 'lives left!')
+      if lives == 0:
+        end_of_game = True #myexplanation: This is like a flag statement instead of break
+        print('You Lost!!')
+        print('The word chosen was: ', chosen_word)
+        # break   #myexplanation: Here break takes while loop exit and prints both print statements. Hence we need to try udemy way end_of_game
+    print(display)
+    
+    
+    if '_' not in display:
+      end_of_game = True
+      print('You Won!!!')
+      print('The word chosen was: ', chosen_word)
+        
+
+#TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
+
--- udemy/100daysofprogramming/day7/day7_6.py
+++ udemy/100daysofprogramming/day7/day7_6.py
@@ -1,13 +1,10 @@
 import random
 from words import word_list
 from art import logo, stages
+# from replit import clear
 
 print(logo)
-#myexplanation: There has to be 7 lives because of 7 drawings and lives not dependent on length of word
-# word_list = ["ardvark", "baboon", "camel"]
 
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
 end_of_game = False
 lives = 6
 
@@ -17,28 +14,29 @@ display = []
 for i in chosen_word:
     display.append(i.replace(i,'_')) #myway
 
-#TODO-2: - If guess is not a letter in the chosen_word,
-#Then reduce 'lives' by 1. 
-#If lives goes down to 0 then the game should stop and it should print "You lose."
-
-# while '_' in display: #myway #myexp: if you use this, u can use break along with end_of_game else with another while statement break is not required
+w_list = []
 while not end_of_game:
     guess = input().lower()
+    w_list.append(guess)
 
     if guess in display:
         print(f"You've already guessed {guess}")
+    if guess == '':
+        print('You need to enter a letter')
+    # if guess in w_list:
+    #     print('You have already guessed this wrong letter!')
     
     for i in range(len(chosen_word)):
         if chosen_word[i] == guess:
             display[i] = chosen_word[i] #or display[i] = guess     
-    if guess not in chosen_word or len(guess) != 1:
+    if guess not in chosen_word or len(guess) > 1:
     # else: #else wont work here only if does
       lives -= 1
-      print('Wrong! You have:', lives, 'lives left!')
+      print(f'Wrong! You have: {lives} lives left!')
       if lives == 0:
         end_of_game = True #myexplanation: This is like a flag statement instead of break
         print('You Lost!!')
-        print('The word chosen was: ', chosen_word)
+        print(f'The word chosen was: {chosen_word}')
         # break   #myexplanation: Here break takes while loop exit and prints both print statements. Hence we need to try udemy way end_of_game
     print(display)
     
@@ -46,7 +44,7 @@ while not end_of_game:
     if '_' not in display:
       end_of_game = True
       print('You Won!!!')
-      print('The word chosen was: ', chosen_word)
+      print(f'The word chosen was: {chosen_word}')
         
 
 #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
