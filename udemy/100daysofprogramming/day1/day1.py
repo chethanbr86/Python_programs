a129dbf94a0f794fdfe291307e5ec6820a40ee10
chethanbr86 <chethanbr86@gmail.com>
2022-11-21 20:20:23 +0530

    Update day1.py

--- udemy/100daysofprogramming/day1/day1.py
+++ udemy/100daysofprogramming/day1/day1.py
@@ -1,12 +1,12 @@
 #1. Create a greeting for your program.
-name = input('name? ')
+name = input('What is your name? ')
 print("Hello " + name + '. How are you?')
 #2. Ask the user for the city that they grew up in.
-city = input('City?\n')
-print('Where did you grow up? ' + city)
+city = input('In which city were you born?\n')
+print('The city you grew up in was: ' + city)
 #3. Ask the user for the name of a pet.
-pet = input('petname?\n')
-print("What's your fav pet?" + pet)
+pet = input('What is your pet name?\n')
+print("Your favourite petname is: " + pet)
 #4. Combine the name of their city and pet and show them their band name.
 print('Your band name is:\n', pet+name+city)
 #5. Make sure the input cursor shows on a new line, see the example at:
