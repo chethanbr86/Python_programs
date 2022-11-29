42a83670a34de0308b028d933f661dffae61011a
chethanbr86 <chethanbr86@gmail.com>
2022-11-29 19:52:05 +0530

    no change

--- udemy/100daysofprogramming/day4/day4_3.py
+++ udemy/100daysofprogramming/day4/day4_3.py
@@ -5,6 +5,7 @@ cap = [row1, row2, row3]
 print(cap)
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
+# print(position)
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
