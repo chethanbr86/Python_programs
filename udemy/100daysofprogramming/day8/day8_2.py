3528deb110deddeccf33e28973e3e8f9a86fbf42
[chethanbr86] <[chethanbr86@gmail.com]>
2023-01-14 21:08:27 +0530

    8.3

--- udemy/100daysofprogramming/day8/day8_2.py
+++ udemy/100daysofprogramming/day8/day8_2.py
@@ -1,6 +1,10 @@
-def greet(x,y):
+def greet(x,y,z):
     print('Hi', x)
     print('You are from', y)
     print(f'Namaskara {x} from {y}')
+    print(f'your age is {z}')
 
-greet(input(), input())
+# greet(input(), input())
+greet('Chethan', 'Bangalore', 2023-1986)
+greet( 2023-1986, 'Bangalore', 'Chethan')
+greet( z = 2023-1986,  y='Bangalore', x='Chethan')
