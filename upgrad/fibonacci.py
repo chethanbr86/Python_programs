526b97f48e6ae5e9ec0a0c794f1c0c07bb8c6653
chethanbr86 <chethanbr86@gmail.com>
2024-11-27 22:36:37 +0530

    codium

--- upgrad/fibonacci.py
+++ upgrad/fibonacci.py
@@ -11,4 +11,17 @@ for i in range(3,n+1):
     print(fib)
     sl = l
     l = fib
-        
+
+#using codium
+# def fib(n):
+#     fibo=[1]*(n+1)
+#     if n <= 0:
+#         return 0
+#     if n == 1:
+#         return 1
+#     else:
+#         for i in range(2, n+1):
+#             fibo[i]=fibo[i-1] + fibo[i-2]
+#         return fibo[n-1]        
+
+# print(fib(6))
