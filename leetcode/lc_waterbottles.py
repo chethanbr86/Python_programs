47014acf805a3f467fea767641722d2669a21af9
chethanbr86 <chethanbr86@gmail.com>
2024-12-02 16:50:35 +0530

    later

--- leetcode/lc_waterbottles.py
+++ leetcode/lc_waterbottles.py
@@ -1,9 +1,12 @@
-class Water:
-    def water_bottles(self, num_bottle, num_empty):
-        self.num_bottle = num_bottle
-        self.num_empty = num_empty
-        # Water.water_bottles 
-        full = 0
-        empty = 0
+# class Water:
+#     def water_bottles(self, num_bottle, num_empty):
+#         self.num_bottle = num_bottle
+#         self.num_empty = num_empty
+#         full = 0
+#         # full = 
+#         trade = num_bottle//num_empty
+#         # if  
 
+print(11//4)
+print(11%4)
                 
