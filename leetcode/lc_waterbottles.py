e46170ae70a45db653f82400a7e27c701cd8f617
chethanbr86 <chethanbr86@gmail.com>
2024-12-02 16:12:55 +0530

    trying lc

---
+++ leetcode/lc_waterbottles.py
@@ -0,0 +1,9 @@
+class Water:
+    def water_bottles(self, num_bottle, num_empty):
+        self.num_bottle = num_bottle
+        self.num_empty = num_empty
+        # Water.water_bottles 
+        full = 0
+        empty = 0
+
+                
