fe8f9c6d2a3288102329add55fa4cb367dd38389
chethanbr86 <chethanbr86@gmail.com>
2024-12-03 11:28:54 +0530

    trying

--- leetcode/lc_waterbottles.py
+++ leetcode/lc_waterbottles.py
@@ -1,12 +1,12 @@
-# class Water:
-#     def water_bottles(self, num_bottle, num_empty):
-#         self.num_bottle = num_bottle
-#         self.num_empty = num_empty
-#         full = 0
-#         # full = 
-#         trade = num_bottle//num_empty
-#         # if  
+class Water:
+    def water_bottles(num_bottle, num_empty):
+        # self.num_bottle = num_bottle
+        # self.num_empty = num_empty
+        total_empty = num_bottle
+        untradeable = num_bottle%num_empty
+        tradeable = total_empty - untradeable
+        if tradeable > num_empty:
+            num_bottle += 1
+            print(total_empty, num_bottle, num_empty, tradeable, untradeable)
 
-print(11//4)
-print(11%4)
-                
+    water_bottles(10, 4)
