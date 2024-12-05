ec3822e9016eacc4450dee9e0414892284bf8819
chethanbr86 <chethanbr86@gmail.com>
2024-12-05 13:46:40 +0530

    python v2

--- upgrad/Python_Programming/above_Average.py
+++ upgrad/Python_Programming/above_Average.py
@@ -3,7 +3,9 @@
 my_list = [[3,7,6,9,10,8,5], 7.5]
 m = my_list[0]
 n = my_list[1]
+
 #1st time
+
 # def sumoflist(m):
 #     total = 0
 #     for i in m:
@@ -20,12 +22,37 @@ n = my_list[1]
 #     print(False)
 
 # 2nd time:
+
 #easy way
-t = sum(m)/len(m)
-print(t)
-if n > t:
-    print(f'{n} is greater than {t:.2f}.')
-else:
-    print('No')
+def avg_calculation(list2):
+    t = sum(list2)/len(list2)
+    print(t)
+    if n > t:
+        print(f'{n} is greater than {t}.')
+    else:
+        print('No')
+
+avg_calculation(m)
 
 #difficult way - calculate avg and sum
+
+# Note: Avoid range(len(list)) unless you need fine-grained control over indices or are working with multiple lists.
+
+# total = 0
+# for i in range(len(m)):
+#     total = total + m[i]
+# print(total)
+# class Calculation:
+
+def avg_cal(list1):        
+    total = 0
+    avg = 0
+    for i in list1:
+        total = total + i
+        avg = total/len(list1)
+    return avg        
+
+if n > avg_cal(m):
+    print(f'{n} is greater than {avg_cal(m)}.')
+else:
+    print('No')
---
+++ youtube/guess_number.py
@@ -0,0 +1,53 @@
+import random
+# play = input('wanna play, press y? ').lower().strip()
+# if play != 'y':
+#     quit()
+
+# Note: computer_gen = random.randrange(11) # 11 not included and starts from 0, also randint(0,11) (11 included) can be used but have to give 2 arguments
+# user_input = int(input('Guess a number between 0 to 10: '))
+
+#Type1: using quit()
+# user_input = input('Guess a number between 0 to 10: ')
+
+# if user_input.isdigit():
+#     user_input = int(user_input)
+#     if user_input > 10 or user_input < 0:
+#         print('Should be between 0 and 10 both included!')
+#         quit()
+# else:
+#     print('only digits allowed')
+#     quit()
+# print(f'User input: {user_input}')
+# computer_gen = random.randrange(0,user_input)
+# print(f'Computer input: {computer_gen}')
+
+#Type2: using while loop with continue and break
+# while True: #another way
+#     try:
+#         user_input = int(input("Enter a number: "))
+#         break  # Exit the loop if input is valid
+#     except ValueError:
+#         print("Error: That's not a valid number. Please enter an integer.")
+
+while True:
+    user_input = input("Enter a number between 0 and 10: ")
+
+    if user_input.isdigit():        
+        user_input = int(user_input)
+        if user_input >= 0 and user_input <= 10:
+            print("Let's Play!")
+        else:
+            print('should be between 0 and 10')
+            continue
+    else:
+        print('only digits allowed')
+        continue
+
+    computer_gen = random.randrange(0,11)
+    print(f'Computer input: {computer_gen}')
+
+    if user_input == computer_gen:
+        print(f"user_input: {user_input} and computer_gen: {computer_gen} are matching.")
+        break
+    else:
+        print(f"user_input: {user_input} and computer_gen: {computer_gen} are not matching!")
