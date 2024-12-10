1146eb777d4bd6ad3eb324b4a911ad5cc06aea7e
chethanbr86 <chethanbr86@gmail.com>
2024-12-10 13:30:43 +0530

    v3

---
+++ my_programs/rich_demo.py
@@ -0,0 +1,7 @@
+#1
+from rich import print
+from rich.panel import Panel
+
+print(Panel("Hello, [red]World!", title="Welcome", subtitle="Thank you"))
+
+# print("Hello, [red]World!", end="Thank you")
