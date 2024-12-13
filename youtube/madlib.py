445cf31dd32c5f062f601369ea232c205dc6a78f
chethanbr86 <chethanbr86@gmail.com>
2024-12-13 17:20:02 +0530

    python v7

--- youtube/madlib.py
+++ youtube/madlib.py
@@ -11,10 +11,34 @@
 #         story = story.replace(word, input('Enter a verb: '))
 #     elif word == 'ADVERB':
 #         story = story.replace(word, input('Enter an adverb: '))
-
 # print(story)
 
 #youtube way
-with open('story_madlib.txt', 'r') as f:
+with open('youtube/story_madlib.txt', 'r') as f:
     story = f.read()
 
+words = set() #to avoid duplicates if this was a list
+start_of_word = -1
+
+target_start = '<'
+target_end = '>'
+
+for i, char in enumerate(story):
+    if char == target_start:
+        start_of_word = i
+    elif char == target_end and start_of_word != -1:
+        # words.append(story[start_of_word+1:i])
+        word = story[start_of_word: i+1]
+        words.add(word)
+        start_of_word = -1
+print(words)
+
+answers = {}
+for word in words:
+    ans = input(f'Enter a {word}: ')
+    answers[word] = ans
+print(answers)
+
+for word in words:
+    story = story.replace(word, answers[word])
+print(story)
