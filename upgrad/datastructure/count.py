bcac51419911900ccca80d3e5a3118b511b6bd0d
chethanbr86 <chethanbr86@gmail.com>
2024-12-17 09:30:22 +0530

    python v12

--- upgrad/datastructure/count.py
+++ upgrad/datastructure/count.py
@@ -1,5 +1,5 @@
 #count how many elements in the list
-t = [0,1,1,2,3,0,5]
+t = [0,1,2,2,3,0,5,7,6,5,2]
 hist = {}
 for x in t:
     hist[x] = hist.get(x, 0) + 1
--- upgrad/datastructure/dictonary.py
+++ upgrad/datastructure/dictonary.py
@@ -1,26 +1,26 @@
-games = {'chet':100, 'indu':10, 'sha':35, 'bhagu': 15}
+games = {'Mike':100, 'Link':10, 'Sucre':35, 'Sara': 15}
 
 #By default, python gives keys when not stated particularly
 for game in games:
-    print(game)
+    print(f'1: {game}')
 
 #To get values
 for game in games:
-    print(games[game])
+    print(f'2: {games[game]}')
 
 #You can mention particularly keys
 for key in games.keys():
-    print(key)
+    print(f'3: {key}')
 
 #You can mention particularly values
 for val in games.values():
-    print(val)
+    print(f'4: {val}')
 
 #Or both
 for key, val in games.items():
-    print(key, val)
+    print(f'5: {key}, {val}')
 
 #get method
 for item in games:
-    k = games.get('chet')
-print(k)
+    k = games.get('Sara') #if key exists, value will be printed
+print(f'6: {k}')
