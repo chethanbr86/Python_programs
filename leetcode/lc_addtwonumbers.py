6e16e2ee1ef2d008dbe5ccdd6ecc0bec0c0fda93
Chethan BR <chethanbr86@gmail.com>
2025-02-02 20:47:31 +0530

    python v24

--- leetcode/lc_addtwonumbers.py
+++ leetcode/lc_addtwonumbers.py
@@ -1,4 +1,4 @@
-l1 = [1,0,8]
+l1 = [1,0,8,5]
 l2 = [6,9,1]
 #output = [7,9,8]
 
@@ -21,12 +21,16 @@ def addNum(l1,l2):
     # t2 = 0
     if len(l1) < len(l2):
         l1.append(0)
-    else:
+    elif len(l1) > len(l2):
         l2.append(0)
+    else:
+        l1 = l1
+        l2 = l2
     print(l1,l2)
     for i in range(len(l1)):
         total.append(l1[i]+l2[i])
     return total
+
 #in both cases what if l1 == l2?
 #And how to know whats the len of linked list to split input and defferentiate between l1 and l2
 
@@ -34,4 +38,3 @@ def addNum(l1,l2):
 # l2 = list(map(int,input().split(',')))
 # print(l1)
 print(addNum(l1,l2))
-
