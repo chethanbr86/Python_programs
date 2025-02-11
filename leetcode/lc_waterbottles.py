71e92edf1057e25892355481d7dc616c9418f95b
chethanbr86 <chethanbr86@gmail.com>
2025-02-11 22:28:18 +0530

    py v27 - not solved water problem

--- leetcode/lc_waterbottles.py
+++ leetcode/lc_waterbottles.py
@@ -1,11 +1,11 @@
-numBottles = 10
-numExchange = 3
+numBottles = 15
+numExchange = 4
 
-total_full = numBottles
-total_exchange = numBottles // numExchange
-while total_exchange != 0:    
+total_full = 0
+total_exchange = 0
+while total_exchange < numExchange:    
     total_full = numBottles + (numBottles // numExchange)
-    total_exchange = (numBottles // numExchange) + (numBottles % numExchange)
+    total_exchange = (total_full // numExchange) + (total_full % numExchange)
 
 print(total_full, total_exchange)
 #not complete
