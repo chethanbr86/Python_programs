d61ec448c85e87c674d59f68db9e6e86bf9c0f5a
chethanbr86 <chethanbr86@gmail.com>
2025-02-21 15:09:22 +0530

    py v33

--- leetcode/lc_prodknum.py
+++ leetcode/lc_prodknum.py
@@ -4,27 +4,27 @@ class ProductOfNumbers:
     def __init__(self):
         # Initialize the product list with 1 to handle multiplication logic
         self.prefix_product = [1]
-        self.size = 0
+        self.index = 0
 
     def add(self, num: int):
         if num == 0:
             # If num is 0, reset the cumulative products since multiplication
             # with 0 invalidates previous products
             self.prefix_product = [1]
-            self.size = 0
+            self.index = 0
         else:
             # Append the cumulative product of the current number with the last
             # product
-            self.prefix_product.append(self.prefix_product[self.size] * num)
-            self.size += 1
+            self.prefix_product.append(self.prefix_product[self.index] * num)
+            self.index += 1
 
     def getProduct(self, k: int) -> int:
-        # Check if the requested product length exceeds the size of the valid
+        # Check if the requested product length exceeds the index of the valid
         # product list
-        if k > self.size:
+        if k > self.index:
             return 0
         # Compute the product of the last k elements using division
-        return self.prefix_product[self.size] // self.prefix_product[self.size - k]
+        return self.prefix_product[self.index] // self.prefix_product[self.index - k]
 
 # multiple input in a line
 obj = ProductOfNumbers()
