270a6cd877d06afdf7a9dab1b1128dafbc336799
chethanbr86 <chethanbr86@gmail.com>
2025-02-21 10:23:44 +0530

    py v31 - test

--- my_programs/test.py
+++ my_programs/test.py
@@ -1,7 +1,19 @@
-bank_balances = {}
-for bank, balance in ('', 0), ('hbank', 20), ('ibank', 25):
-    bank_balances[bank] = bank_balances.get(bank, 0) - balance  # removed - above in from_bank_summary and added - here
-for bank, balance in ('', 10), ('hbank', 40), ('ibank', 50):
-    bank_balances[bank] = bank_balances.get(bank, 0) + balance
-print(bank_balances.items())
-#debug in thonny
+# bank_balances = {}
+# for bank, balance in ('', 0), ('hbank', 20), ('ibank', 25):
+#     bank_balances[bank] = bank_balances.get(bank, 0) - balance  # removed - above in from_bank_summary and added - here
+# for bank, balance in ('', 10), ('hbank', 40), ('ibank', 50):
+#     bank_balances[bank] = bank_balances.get(bank, 0) + balance
+# print(bank_balances.items())
+# #debug in thonny
+
+numbers = input("Enter numbers separated by space: ").split()
+numbers = [int(x) for x in numbers]
+print(numbers)
+
+my_list = [1]
+size = 0
+
+my_list.append(my_list[size]*numbers)
+size += 1
+
+print(my_list)
