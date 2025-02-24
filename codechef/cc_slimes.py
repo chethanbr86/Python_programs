58920a2cf7a8447fe63faed383bdd65eae78fbfe
chethanbr86 <chethanbr86@gmail.com>
2025-02-24 20:20:02 +0530

    py v36

--- codechef/cc_slimes.py
+++ codechef/cc_slimes.py
@@ -5,5 +5,14 @@ for i in range(t):
     n = int(input().strip())
     a = list(map(int,input().strip().split(',')))
     list_a.append(a)
-print(list_a)
+# print(list_a)
 
+for sub_list in range(len(list_a)):
+    for l in range(len(sub_list)):
+        if l[0] > l[1]:
+            sub_list.pop(l[0])
+        else:
+            l[1] - l[0]
+        
+print(list_a)
+#not solved
