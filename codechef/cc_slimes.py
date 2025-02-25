3a5bd12081f4f212105cdf1c8e5902f3403978ce
chethanbr86 <chethanbr86@gmail.com>
2025-02-25 20:38:29 +0530

    py v37

--- codechef/cc_slimes.py
+++ codechef/cc_slimes.py
@@ -5,14 +5,29 @@ for i in range(t):
     n = int(input().strip())
     a = list(map(int,input().strip().split(',')))
     list_a.append(a)
-# print(list_a)
-
-for sub_list in range(len(list_a)):
-    for l in range(len(sub_list)):
-        if l[0] > l[1]:
-            sub_list.pop(l[0])
-        else:
-            l[1] - l[0]
-        
 print(list_a)
-#not solved
+
+# for i in list_a:
+#     max_element = i[0]
+#     for j in i:
+#         if j > max_element:
+#             max_element = j
+#         else:            
+#             i.remove(j)
+#     print(max_element)
+
+for i in list_a:
+    # max_element = i[0]
+    for j in range(len(i)-1):
+        if i[j] > i[j+1]:
+            i[j] - i[j+1]
+            i.remove(i[j+1])
+        # else:            
+        #     j[1] - j[0]
+        #     i.remove(j[1])
+    # print(max_element)
+
+print(list_a)
+
+#not completed
+        
