5496509710e74c827aa57eddfd1cabbf1a6c9dc6
chethanbr86 <chethanbr86@gmail.com>
2025-02-27 21:02:35 +0530

    py v41

--- my_programs/for_loop_skeleton.py
+++ my_programs/for_loop_skeleton.py
@@ -1,5 +1,5 @@
 # 1st type:
-list_a = [[1,2],[1,2,3],[1,100,10]]
+# list_a = [[1,2],[1,2,3],[1,100,10]]
 
 # for i in list_a:
 #     for j in range(len(i)-1): #this is applicable to nested lists, throws error at single list, also range(len(i)-1) refers to index
@@ -19,6 +19,14 @@ list_a = [[1,2],[1,2,3],[1,100,10]]
 #         print(i,j)
 
 # IMP: Making nested list into separate lists
-for i in range(len(list_a)):
-    sub_list = list_a[i]
-    print(sub_list)    
+# for i in range(len(list_a)):
+#     sub_list = list_a[i]
+#     print(sub_list)    
+
+#checking reverse index in for loop
+list_b = [1,2,3,4,5,6,7,8]
+
+for i in range(len(list_b)):
+    for j in range(i-2,-1,-1):
+        print(f'i:{i}, j:{j}')
+
