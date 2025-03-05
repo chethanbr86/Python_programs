84f2fa2c7c108e946d58ccb5d7ad4635b4011897
chethanbr86 <chethanbr86@gmail.com>
2025-03-05 13:49:02 +0530

    py v48 - codechef - pronouns

--- codechef/cc_pronouns.py
+++ codechef/cc_pronouns.py
@@ -12,15 +12,29 @@ else:
     def index_yes():
         index = []
         for i,j in enumerate(S):    
-            if j in v:        
-                
+            if j in v:                        
                 index.append(i)      
-        print('No')
+        if not index:
+            print('No')
         return index
 
     m = index_yes()
     print(m)
 
-    for i,j in enumerate(m):
-        print(m[i])
-#to be continued
+    #Difference between each list element
+    diff_list = []
+    def prefix_sum(list_b):
+        for i in range(1,len(list_b)):
+            diff_list.append(list_b[i] - list_b[i-1])
+        return diff_list
+    
+    n = prefix_sum(m)
+    print(n)
+    
+    for i in diff_list:
+        if i > 3:
+            print('No')
+    else:
+        print('Yes')
+#i am getting solution but it is taking difference between vowel index, i also need to consider, differnt from 0 index to first vowel too
+
