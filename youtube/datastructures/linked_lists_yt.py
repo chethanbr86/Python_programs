5e8443a38d92c3aecc6e542e0356e50b0a1964f4
chethanbr86 <chethanbr86@gmail.com>
2025-03-05 12:15:07 +0530

    py v47

--- youtube/datastructures/linked_lists_yt.py
+++ youtube/datastructures/linked_lists_yt.py
@@ -13,6 +13,7 @@ class LinkedList: #LinkedList is another class which utilizes the Node class
     def __repr__(self):
         pass
 
+    #O(n) 
     def __contains__(self): #checks if a value exists in the list
         pass
 
@@ -35,9 +36,28 @@ class LinkedList: #LinkedList is another class which utilizes the Node class
         first_node.next = self.head 
         self.head = first_node
 
+    # O(n)
     def insert(self, value, index): #inserting value at a specific index
         if index == 0: #If inserting at empty list
             self.prepend(value) #you can append also but it will have O(n) time complexity
+        else: #if the list is empty and if I am trying to insert at an index that is not 0 then the below code will throw error and hence raising exception
+            if self.head is None:
+                raise ValueError("Index out of bounds")
+            else: #if head is not none
+                last = self.head #head position is confirmed now (not assigning value here, only confirming head position)
+
+                #Even though last contains head now, we need to iterate till the index position where we need to insert
+                for i in range(index-1):
+                    if last.next is None:
+                        raise ValueError("Index out of bounds")
+                    last = last.next #now we got the last node after which we need to insert (25 in drawio)
+
+                #In the below 3 lines, only the nodes are re-assigned as shown in insert.drawio
+                new_node = Node(value) #position 30 in insert.drawio
+                new_node.next = last.next #position 35 in insert.drawio
+                last.next = new_node
+
+
 
     def delete(self, value):
         pass
