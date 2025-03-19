6eb128f6600583f610b740b8a0205dcac3c7ce6b
chethanbr86 <chethanbr86@gmail.com>
2025-03-19 12:26:41 +0530

    py v61 - least_difference - solved

--- my_programs/lessons.py
+++ my_programs/lessons.py
@@ -15,3 +15,24 @@ input can be taken in 2 ways:
 #     while True:
 #         List1 = list(map(int, input().split()))
 #         List2 = list(map(int, input().split()))
+
+'''
+When you need to iterate through same index of both lists, you don't need to include nested loops.
+You can refer index/value of another list with same i.
+'''
+# list1 = [0.896761,1.000000,1.000000,1.000000,1.000000,0.967020,0.896536,0.915352,0.928808,0.924341,1.000000]
+# list2 = [0.891260,0.725591,0.863516,0.875115,0.864161,0.885342,0.892017,0.872830,0.879555,0.878611,0.866223]
+# # List to store the results
+# result = []
+
+# # Iterate through both lists
+# for i in range(len(list1)):
+#     if list1[i] > list2[i] and list1[i] != 1:
+#         difference = round(list1[i] - list2[i], 4)
+#         result.append((list1[i], list2[i], difference))
+
+# print(result)
+# # Sort the result by the least difference
+# result.sort(key=lambda x: x[2])
+
+# print(result)
--- projects/least_difference.py
+++ projects/least_difference.py
@@ -5,16 +5,17 @@ list2 = [0.891260,0.725591,0.863516,0.875115,0.864161,0.885342,0.892017,0.872830
 '''Need 2 outputs from 2 lists where difference between corresponding index values are least but the first value is greater than second and first value has a better score if not highest.
 Meaning 1st priority is least difference and 2nd priority is highest value'''
 
-   
+# List to store the results
+result = []
 
-list3 = []
-for i in range(len(list1)-1):
-    for j in range(len(list2)-1):
-        if list1[i] < list2[j] or list1[i] == 1:
-            i += 1
-            j += 1
-        else:
-            if i == j:
-                list3.append([list1[i],list2[j]])
-            # break
-print(list3)
+# Iterate through both lists
+for i in range(len(list1)):
+    if list1[i] > list2[i] and list1[i] != 1:
+        difference = round(list1[i] - list2[i], 4)
+        result.append((list1[i], list2[i], difference))
+
+print(result)
+# Sort the result by the least difference
+result.sort(key=lambda x: x[2])
+
+print(result)
