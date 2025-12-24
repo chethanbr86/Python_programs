fd892b999094d367b040a39f641cd1553489ad49
chethanbr86 <chethanbr86@gmail.com>
2025-12-24 20:09:48 +0530

    feat: add Python script to print two variations of diamond patterns.

--- upgrad/Python_Programming/diamond_pattern.py
+++ upgrad/Python_Programming/diamond_pattern.py
@@ -15,3 +15,25 @@ for i in range(n):
     print(' '*(n-i-1)+('* ')*(i+1)) #or ('*'+' ')
 for j in range(n-1):
    print(' '*(j+1)+('* ')*(n-j-1))
+
+'''
+
+    *
+   *_*
+  *_*_*
+ *_*_*_*
+*_*_*_*_*
+
+'''
+
+#please take input here
+n = int(input())
+
+#start writing your code here
+if 1<=n<=20:
+    for i in range(1,n+1):
+        for j in range(n-i):
+            print(' ',end='')
+        for k in range(i-1):
+            print('*_',end='')
+        print('*')
