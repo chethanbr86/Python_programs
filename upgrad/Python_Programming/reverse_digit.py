1123e246c410a468caedea0a437ff26af57dc0a3
chethanbr86 <chethanbr86@gmail.com>
2025-12-24 20:06:18 +0530

    explain

--- upgrad/Python_Programming/reverse_digit.py
+++ upgrad/Python_Programming/reverse_digit.py
@@ -2,6 +2,6 @@ n = 24316
 
 rev = 0
 while n > 0:
-    rev = rev*10 + n%10
-    n = n//10
+    rev = n%10 + rev*10 #n%10 is the last digit of n
+    n = n//10 #n//10 is the remaining digits of n
 print(rev)
