8dfb06bbaafab1fc730db8592c49ed19c09707df
chethanbr86 <chethanbr86@gmail.com>
2026-05-08 21:46:24 +0530

    pyv62

---
+++ daily_coding/dc1854_string.py
@@ -0,0 +1,14 @@
+str_set = 'figehaeci'
+sub_str = {'a','e','i'}
+
+print(type(sub_str))
+
+for i in sub_str:
+    if i in sub_str:
+        print('Yes')
+    else:
+        print('No')
+
+for i in sub_str:
+    if i in sub_str:
+        pass
