a6ecb98b89c96419ac9d340e15f86bd378dabecc
chethanbr86 <chethanbr86@gmail.com>
2026-05-09 22:43:33 +0530

    pyv63 - i still got it figuring out by myself, just prepare and you are good to go

--- daily_coding/dc1854_string.py
+++ daily_coding/dc1854_string.py
@@ -1,14 +1,44 @@
 str_set = 'figehaeci'
 sub_str = {'a','e','i'}
+# sub_list = list(sub_str)
+sub_list = ['a','e','i']
 
 print(type(sub_str))
 
-for i in sub_str:
-    if i in sub_str:
-        print('Yes')
-    else:
-        print('No')
+#For loop testing
+# for i in sub_str:
+#     if i in sub_str:
+#         print('Yes',i)
+#     else:
+#         print('No',i)
 
-for i in sub_str:
-    if i in sub_str:
-        pass
+#This works but go to next one for changes
+# count_outer = 0
+# count_inner = 0
+# #Since we need an index from which everything needs to be printed, lets use range
+# for i in range(len(sub_list)):    
+#     count_outer += 1
+#     for j in range(len(str_set)):
+#         count_inner += 1
+#         if sub_list[i] == str_set[j]:
+#             print('Yes',str_set[i:],i,j)
+#         # else:
+#         #     print('No')
+# print(count_inner)
+# print(count_outer)
+
+count_outer = 0
+count_inner = 0
+#Since we need an index from which everything needs to be printed, lets use range
+for i in range(len(sub_list)):    
+    count_outer += 1
+    for j in range(len(str_set)):
+        count_inner += 1
+        if sub_list[i] == str_set[j]:
+            break
+print('Yes',str_set[j:],i,j)
+        # else:
+        #     print('No')
+print(count_inner)
+print(count_outer)
+#This one actually works but not as the problem wanted, check once.
