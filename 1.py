bdc2b9d9a6a27366df93e17e5c24c0d122bb6d3a
chethanbr86 <chethanbr86@gmail.com>
2026-06-04 22:47:33 +0530

    forgot changes

---
+++ 1.py
@@ -0,0 +1,12 @@
+def reverse_word(word):
+    reversed = ""
+    for letter in word:
+        reversed = letter + reversed
+    return reversed
+
+def check_all_palindromes(arr):
+    if arr[0] == reverse_word(arr[0]):
+        if arr[1] == reverse_word(arr[1]):
+            if arr[2] == reverse_word(arr[2]):
+                return True
+    return False
